a2812c0fc6f07ff7b0c4d368e06f5be7fad6ac4f
Tim Peters <mail@darksecond.nl>
2021-04-04 14:58:50 +0200

    Switch to shaka player ui & clean up stuff

--- assets/player.js
+++ assets/player.js
@@ -1,27 +1,12 @@
 async function init() {
-  const source = document.querySelector('meta[name=video-dash]').content;
-  const video = document.querySelector('#video');
-  let options = {
-    controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'settings', 'pip', 'fullscreen'],
-    settings: ['quality', 'loop'],
+  const video = document.querySelector('video');
+  const ui = video['ui'];
+  const config = {
+    'controlPanelElements': ['play_pause', 'time_and_duration', 'spacer', 'mute', 'volume', 'overflow_menu', 'fullscreen'],
+    'overflowMenuButtons': ['loop', 'quality', 'picture_in_picture'],
+    'addBigPlayButton': false,
   };
-  const player = new Plyr(video, options);
-  window.player = player;
-
-  if (shaka.Player.isBrowserSupported()) {
-    shaka.polyfill.installAll();
-    const shakaInstance = new shaka.Player(video);
-    window.shakaInstance = shakaInstance;
-    shakaInstance.configure({
-      streaming: {
-        useNativeHlsOnSafari: false,
-      },
-    });
-
-    shakaInstance.load(source);
-    
-    setInterval(()=>{player.speed=1}, 1000); // Reset speed every 1 second(s), in case it (somehow) gets screwed up
-  }
+  ui.configure(config);
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('shaka-ui-loaded', init);
